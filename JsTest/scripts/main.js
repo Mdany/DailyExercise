9eca26758bb5cfa58f485ae2433597e00ac9bb71
王晨宇 <wangchenyu100@gmail.com>
2016-03-14 17:11:52 +0800

    javaScrip test

---
+++ JsTest/scripts/main.js
@@ -0,0 +1,11 @@
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
